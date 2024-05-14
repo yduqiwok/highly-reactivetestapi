2e53dea8c6b6f54009c15556b06e3930e0a865b6
yduqiwok <gs1salimcuk@hotmail.com>
2024-05-14 22:25:54 +0700

    First commit

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-function canJump(nums) {
-  let maxJump = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (i > maxJump) return false;
-    maxJump = Math.max(maxJump, i + nums[i]);
-    if (maxJump >= nums.length - 1) return true;
-  }
-  return false;
-}
+const maxN = (arr, n = 1) => [...arr].sort((a, b) => b - a).slice(0, n);
